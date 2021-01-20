10fc34473fc5217374e61f3c4f549a8a423624f4
Luis German Esquivel Grados <lgeldragondorado@gmail.com>
2021-01-20 00:42:15 -0500

    se corrigio el filtro de area

--- src/Componentes/DemandaLaboral.js
+++ src/Componentes/DemandaLaboral.js
@@ -158,7 +158,7 @@ class DemandaLaboral extends React.Component {
 
   handleChangeSelectArea = (estado) => {
     this.setState({
-      area_auctal: { value: estado.value, label: estado.label }
+      area_actual: { value: estado.value, label: estado.label }
     });
   }
 
